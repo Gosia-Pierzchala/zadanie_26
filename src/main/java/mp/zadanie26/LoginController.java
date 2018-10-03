0b1a246eda46d6d2f24233a436bd99f030195952
Gosia-Pierzchala <gk.kalinowska@gmail.com>
2018-10-03 22:24:07 +0200

    wersja 6.

--- src/main/java/mp/zadanie26/LoginController.java
+++ src/main/java/mp/zadanie26/LoginController.java
@@ -9,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class LoginController {
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private UserRoleRepository userRoleRepository;
 
-    public LoginController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public LoginController(UserRepository userRepository, PasswordEncoder passwordEncoder, UserRoleRepository userRoleRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.userRoleRepository = userRoleRepository;
     }
 
     @GetMapping("/")
@@ -40,17 +43,21 @@ public class LoginController {
     }
 
     @GetMapping("/editUser")
-    public String edit(Principal principal, Model model) {
-        String username = principal.getName();
-        User user = userRepository.findByUsername(username);
-        model.addAttribute("user", user);
-        return "editUser";
+    public String edit(@RequestParam Long id, Model model) {
+        Optional<User> optional = userRepository.findById(id);
+        if(optional.isPresent()) {
+            User user = optional.get();
+            model.addAttribute("user", user);
+            return "editUser";
+        } else {
+            return "redirect:/";
+        }
     }
 
     @PostMapping("/editUser")
     public String edit(User user) {
         userRepository.save(user);
-        return "redirect:/";
+        return "redirect:/login";
     }
 
     @GetMapping("/users")
@@ -59,4 +66,23 @@ public class LoginController {
         model.addAttribute("users", list);
         return "users";
     }
+
+    @GetMapping("/user")
+    public String info(@RequestParam Long id, Model model) {
+        Optional<User> optional = userRepository.findById(id);
+        if(optional.isPresent()) {
+            User user = optional.get();
+            model.addAttribute("user", user);
+            List<UserRole> list = userRoleRepository.findAll();
+            for (int i = 0; i < list.size(); i++) {
+                UserRole userRole = list.get(i);
+                if(user.getUsername().equals(userRole.getUsername())){
+                    model.addAttribute("userRole", userRole);
+                }
+            }
+            return "userRole";
+        }
+        else return "redirect:/";
+    }
+
 }
